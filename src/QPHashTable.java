56953f6df250fa301ef3fe3ad0f8feaea3850294
Maya Farber Brodsky <maya@farberbrodsky.com>
2020-06-09 11:31:38 +0300

    Add AQPHashTable.java, QPHashTable.java implementation

--- src/QPHashTable.java
+++ src/QPHashTable.java
@@ -1,15 +1,17 @@
 import java.util.Random;
 
 public class QPHashTable extends OAHashTable {
+    private final ModHash modhash;
+    private final int m;
 
-	public QPHashTable(int m, long p) {
-		super(m);
-		// TODO Complete hash table constructor.
-	}
-	
-	@Override
-	public int Hash(long x, int i) {
-		// TODO implement hash function
-		return 0;
-	}
+    public QPHashTable(int m, long p) {
+        super(m);
+        this.m = m;
+        modhash = ModHash.GetFunc(m, p);
+    }
+
+    @Override
+    public int Hash(long x, int i) {
+        return (modhash.Hash(x) + i * i) % m;
+    }
 }
