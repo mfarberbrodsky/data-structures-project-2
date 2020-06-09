ad7a0064aa8d51633987b9f81ab6bc2ae5a228cb
Maya Farber Brodsky <maya@farberbrodsky.com>
2020-06-09 10:58:27 +0300

    Add ModHash.java implementation

--- src/ModHash.java
+++ src/ModHash.java
@@ -1,13 +1,29 @@
+import java.util.Random;
 
 public class ModHash {
-	
-	public static ModHash GetFunc(int m, long p){
-		// TODO implement random choice of mod function from family
-		return null;
-	}
-	
-	public int Hash(long key) {
-		// TODO implement hash function
-		return 0;
-	}
+    private final int m;
+    private final long p;
+    private final long a;
+    private final long b;
+
+    private ModHash(int m, long p, long a, long b) {
+        this.m = m;
+        this.p = p;
+        this.a = a;
+        this.b = b;
+    }
+
+    public static ModHash GetFunc(int m, long p) {
+        // We will maybe have to change this,
+        // not sure if this is the best way to generate random long numbers
+
+        Random random = new Random();
+        long a = Math.abs(random.nextLong()) % (p - 1) + 1;
+        long b = Math.abs(random.nextLong()) % p;
+        return new ModHash(m, p, a, b);
+    }
+
+    public int Hash(long key) {
+        return (int) (((a * key + b) % p) % m);
+    }
 }
